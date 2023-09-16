6736e99d47dca45d2ed7acdb28537fe71e0724cc
Sannidhi-2003 <shettysannidhi17@gmail.com>
2023-09-16 16:33:06 +0530

    Commit

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-function findSolution(){
-    // Expression
+function findSolution(expression){
+    let sol;
+    for(let i=0;i<expression.length;i++){
+        if(i!="+" ||i!="-" ||i!="*"||i!="/"){
+
+        }
+    }
 }
+
+function prec(oprt){
+ if(oprt=="(")
+ return 4;
+else if(oprt=="^")
+return 3
+ else if(oprt=="*"||oprt=="/")
+ return 2;
+ if(oprt=="+"||oprt=="-")
+ return 1;
+ if(oprt==")")
+ return 0;
+}
+
